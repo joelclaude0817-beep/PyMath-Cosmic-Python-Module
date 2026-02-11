07989dd274ae6481fb4869c7c3ebf8a58ce25951
Joel <joelclaude0817@gmail.com>
2026-02-11 14:56:13 +0800

    Add basic math operations to pymath_cosmic.py

---
+++ pymath_cosmic.py
@@ -0,0 +1,21 @@
+"""
+PyMath - A simple custom Python math library by Cosmic.
+
+Provides basic math operations:
+- Addition
+- Subtraction
+- Multiplication
+- Division
+"""
+
+def getMathPlus(a, b):
+    return a + b
+
+def getMathMinus(a, b):
+    return a - b
+
+def getMathMulti(a, b):
+    return a * b
+
+def getMathDivide(a, b):
+    return a / b
